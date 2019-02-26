7bbc742ecd8c21d4d95a0049a6e518a04f43041a
studente <studente@acerMR>
2019-02-26 17:03:16 +0100

    first commit after local commit

--- JavaFirstApplicationGit/src/javafirstapplicationgit/Main.java
+++ JavaFirstApplicationGit/src/javafirstapplicationgit/Main.java
@@ -18,6 +18,7 @@ public class Main {
         // TODO code application logic here
         System.out.println("Hello MaryMary!");
         System.out.println("Hello, Marcy!");       
+        System.out.println("Hello Lucy!");
     }
     
 }
